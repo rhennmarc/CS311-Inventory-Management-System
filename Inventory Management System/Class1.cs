8038032ed3dd554d063bba6ec7e67b63df29abf5
rhennmarc <mercadorhennmarc@gmail.com>
2025-09-19 17:49:44 +0800

    Fixed some errors

--- Inventory Management System/Class1.cs
+++ Inventory Management System/Class1.cs
@@ -3,48 +3,53 @@ using MySql.Data.MySqlClient;
 //for SQL server using System.Data.SqlClient;
 using System.Data;
 
-
 namespace inventory_management
 {
     class Class1
     {
         private string sqlConString;
         public int rowAffected = 0;
+
         public Class1(string server_address, string database, string username, string password)
         {
             //Server = server name(xampp) Uid = username Pwd = password
-            sqlConString = "Server = " + server_address + "; Database = " + database
-                + "; UId = " + username + "; Pwd = " + password + "; CharSet = utf8;";
+            sqlConString = "Server=" + server_address + ";Database=" + database
+                + ";Uid=" + username + ";Pwd=" + password + ";CharSet=utf8;Pooling=true;Max Pool Size=100;";
         }
+
         //select
         public DataTable GetData(string sql)
         {
-            //connection
-            MySqlConnection Sqlcon = new MySqlConnection(sqlConString);
-            //checking if the connection is close
-            if (Sqlcon.State == ConnectionState.Closed) Sqlcon.Open();
-            //creating a command using the connection and the sql query
-            MySqlCommand SQLcom = new MySqlCommand(sql, Sqlcon);
-            //creating the adapter using the created sql command
-            MySqlDataAdapter SQLadap = new MySqlDataAdapter(SQLcom);
-            DataSet ds = new DataSet();
-            //fill the dataset using the adapter
-            SQLadap.Fill(ds);
-            return ds.Tables[0];
+            DataTable dt = new DataTable();
+
+            using (MySqlConnection Sqlcon = new MySqlConnection(sqlConString))
+            {
+                using (MySqlCommand SQLcom = new MySqlCommand(sql, Sqlcon))
+                {
+                    using (MySqlDataAdapter SQLadap = new MySqlDataAdapter(SQLcom))
+                    {
+                        Sqlcon.Open();
+                        SQLadap.Fill(dt);
+                    }
+                }
+            } // ✅ connection auto-closed and disposed here
+
+            return dt;
         }
+
         //insert, update, delete
         public void executeSQL(string sql)
         {
-            //connection
-            MySqlConnection Sqlcon = new MySqlConnection(sqlConString);
-            //open the connection
-            if (Sqlcon.State == ConnectionState.Closed) Sqlcon.Open();
-            //build the sql command using the sql statement and the connection
-            MySqlCommand SQLcom = new MySqlCommand(sql, Sqlcon);
-            //execute the sql command
-            rowAffected = SQLcom.ExecuteNonQuery();
+            using (MySqlConnection Sqlcon = new MySqlConnection(sqlConString))
+            {
+                using (MySqlCommand SQLcom = new MySqlCommand(sql, Sqlcon))
+                {
+                    Sqlcon.Open();
+                    rowAffected = SQLcom.ExecuteNonQuery();
+                }
+            } // ✅ connection auto-closed and disposed here
         }
-        public string SqlConString { get; set; }
 
+        public string SqlConString { get; set; }
     }
 }
